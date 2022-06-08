5038d83bc722d75ee95424a2ac7933a1714da8ff
Marino Sabic <msabic18@gmail.com>
2022-06-08 19:55:38 +0200

    added sass

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import '../styles/globals.scss'
 
 function MyApp({ Component, pageProps }: AppProps) {
     return <Component {...pageProps} />
